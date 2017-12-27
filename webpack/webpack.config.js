fad56784bc21a8052fa5e625c26a6df072dd01f3
Alexey Batuhtin <full.naked.eye@gmail.com>
2017-12-27 14:57:16 +0700

    feat: add webpack config

---
+++ webpack/webpack.config.js
@@ -0,0 +1,108 @@
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const rootDir = path.resolve(`${__dirname}/..`);
+
+const config = {
+  cache: true,
+  entry: {
+    app: `${rootDir}/src/core/app.js`
+  },
+  devtool: 'cheap-module-source-map',
+  output: {
+    filename: '[name].bundle.js',
+    sourceMapFilename: '[file].map',
+    path: `${rootDir}/dist`,
+    publicPath: '/'
+  },
+  resolve: {
+    modules: [
+      `${rootDir}/src`,
+      `${rootDir}/node_modules`
+    ] /* ,
+    alias: {
+      fonts: `${rootDir}/core/styles/fonts`
+    } */
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel-loader?cacheDirectory=true'
+      },
+      {
+        test: /\.css$/,
+        exclude: /(components\/ui\/ui\/src)/,
+        use: [
+          'style-loader',
+          'css-loader',
+          {
+            loader: 'postcss-loader',
+            options: {
+              plugins: [require('postcss-import')(), require('postcss-cssnext')()]
+            }
+          }
+        ]
+      },
+      {
+        test: /\.css$/,
+        include: /(components\/ui\/ui\/src)/,
+        use: [
+          'style-loader',
+          {
+            loader: 'css-loader',
+            options: {
+              modules: true,
+              sourceMap: true,
+              importLoaders: 1,
+              localIdentName: '[name]__[local]___[hash:base64:5]'
+            }
+          },
+          {
+            loader: 'postcss-loader',
+            options: {
+              plugins: [require('postcss-import')(), require('postcss-cssnext')()]
+            }
+          }
+        ]
+      },
+      { test: /\.(jpg|png|gif)$/, loader: 'file-loader' },
+      { test: /\.(woff|woff2|eot|ttf|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: 'url-loader?limit=100000' }
+    ]
+  },
+  plugins: getPlugins(),
+  devServer: {
+    contentBase: `${rootDir}/dist`,
+    noInfo: false,
+    hot: true,
+    inline: true,
+    historyApiFallback: true,
+    proxy: {
+      /* add google books api */
+    }
+  }
+};
+
+module.exports = config;
+
+function getPlugins() {
+  const ret = [
+    new webpack.NoEmitOnErrorsPlugin(),
+    new webpack.NamedModulesPlugin(),
+    new webpack.HotModuleReplacementPlugin(),
+    new HtmlWebpackPlugin({
+      template: `${rootDir}/src/core/index.html`,
+      inject: 'body' /* ,
+      favicon: `${rootDir}/src/core/favicon.png` */
+    }),
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: JSON.stringify(process.env.NODE_ENV || 'development')
+      }
+    })
+  ];
+
+  return ret;
+}
